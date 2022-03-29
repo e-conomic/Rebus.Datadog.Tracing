765635108ebefd10930c32b075da5520262c1892
Mihaita Bamburic <mihaita.bamburic@visma.com>
2022-03-29 10:21:54 +0300

    Use current style standards

---
+++ src/Rebus.Datadog.Tracing.Tests/TestsBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Rebus.Transport;
+
+namespace Rebus.Datadog.Tracing.Tests
+{
+	public class TestsBase
+	{
+		public int _nbrOfCalls { get; set; }
+
+		protected static ITransactionContext CreateTransactionContext()
+		{
+			var scope = new RebusTransactionScope();
+
+			var transactionContext = scope.TransactionContext;
+			return transactionContext;
+		}
+
+		protected async Task Next()
+		{
+			_nbrOfCalls++;
+			await Task.CompletedTask;
+		}
+	}
+}
